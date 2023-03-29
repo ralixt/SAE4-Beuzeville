ac2a0fa8c5604844d5b11fb9e40c5e62bdd7f1b7
MohamedREHAIL <98965484+MohamedREHAIL@users.noreply.github.com>
2023-03-29 09:42:24 +0200

    databaseupdate

--- beuzeville/src/components/Database.jsx
+++ beuzeville/src/components/Database.jsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
+import { getDatabase, ref, set } from
+'https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
