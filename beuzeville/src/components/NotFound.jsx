e787fc4fcb1601d816ea2917fbc7ebb0f2887ae2
ralixt <98614258+ralixt@users.noreply.github.com>
2023-03-29 13:13:40 +0200

    modification NotFount page

--- beuzeville/src/components/NotFound.jsx
+++ beuzeville/src/components/NotFound.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './../App.css'
+import './../CSS/NotFound.css'
 import {Link} from "react-router-dom";
 
 export function NotFound(props){
     return <>
-        <div>
+        <div id='page404'>
+            <span class="material-symbols-outlined errorIcon">error</span>
             <h1>Erreur 404</h1>
             <p>Oups ! La page que vous demandez n'a pas été trouvée…</p>
-            <Link to="/" className="btn btn-primary">Retourner sur la page d’acceuil</Link>
+            <Link to="/" className="button">Retourner sur la page d’acceuil</Link>
         </div>
 
 
