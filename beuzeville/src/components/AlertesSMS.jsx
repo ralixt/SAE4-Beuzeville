89620676c4f51e3d4b4b2c99c9d6f89c15bb4e90
ralixt <98614258+ralixt@users.noreply.github.com>
2023-04-01 15:36:23 +0200

    finalisation alertesSMS

--- beuzeville/src/components/AlertesSMS.jsx
+++ beuzeville/src/components/AlertesSMS.jsx
@@ -22,30 +22,38 @@ export function AlertesSMS(props){
 
 
 
-    return <> 
+    return <>
         <div id='sms'>
-            <img src={alertesSMSBackground} alt="Fond Alertes SMS" id="alertesSMSBackground"/>
+            <div class="divBackground">
+                <img src={alertesSMSBackground} alt="Fond Alertes SMS" id="alertesSMSBackground"/>
+            </div>
+
             <h1>Alertes SMS</h1>
-            <p>Abonnez-vous pour rester informé des alertes de la ville !</p>
-                <form method="post" action="">
-                    <label for="nom">Nom<span className="obligatoire">*</span></label>
-                    <input name="nom" type="text" placeholder='Nom' value={nom} onChange={e => setNom(e.target.value)}></input>
+            <p id="subtitle">Abonnez-vous pour rester informé des alertes de la ville !</p>
+            <form method="post" action="">
 
-                    <label for="prenom">Prénom<span className="obligatoire">*</span></label>
-                    <input name="prenom" type="text" placeholder='Prénom' value={prenom} onChange={e => setPrenom(e.target.value)}></input>
 
-                    <label for="numTel">Numéro de téléphone<span className="obligatoire">*</span></label>
-                    <input name="numTel" type="text"  placeholder='Numéro de téléphone' value={tel} onChange={e => setTel(e.target.value)}></input>
+                <label for="nom">Nom<span className="obligatoire">*</span></label>
+                <input name="nom" type="text" placeholder='Nom' value={nom} onChange={e => setNom(e.target.value)}></input>
 
+                <label for="prenom">Prénom<span className="obligatoire">*</span></label>
+                <input name="prenom" type="text" placeholder='Prénom' value={prenom} onChange={e => setPrenom(e.target.value)}></input>
+
+                <label for="numTel">Numéro de téléphone<span className="obligatoire">*</span></label>
+                <input name="numTel" type="tel"  pattern="[0-9]{10}" size="10" placeholder='Numéro de téléphone' value={tel} onChange={e => setTel(e.target.value)}></input>
+
+
+
+                <div>
                     <input type="checkbox" id="scales" name="scales" required></input>
+                    <label htmlFor="scales">J'accepte de recevoir les alertes par sms <span className="obligatoire">*</span></label>
+                </div>
 
+                <div id="divButton">
+                    <button className="buttonSMS" type="submit" onClick={send}>Je m'inscris</button>
+                </div>
 
-                    <div>
-                        <label htmlFor="scales">J'accepte de recevoir les alertes par sms <span className="obligatoire">*</span></label>
-                    </div>
-
-                    <button type="submit" onClick={send}>Je m'inscris</button>
-                </form>
+            </form>
         </div>
     </>
 
