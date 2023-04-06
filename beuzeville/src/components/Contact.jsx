2b660186436c9d99f8c58c30174d71db8f5f603d
batos1er <bap.menet@gmail.com>
2023-04-06 15:12:03 +0200

    Contact

--- beuzeville/src/components/Contact.jsx
+++ beuzeville/src/components/Contact.jsx
@@ -37,15 +37,15 @@ export default function Contact(props){
 return <>
     <h1>Contact</h1>
     <div>
-        <form>
+        <form className='size90'>
             <section>
                 <h2>Vos Informations</h2>
-                <div className='Row'>
-                    <div className='Column'>
+                <div className='Row1'>
+                    <div className='Column size40 noMarginLeft'>
                         <label>Nom</label>
                         <input className='InputContact' type="text" name="name" placeholder="Nom"  value={Nom} onChange={e => setNom(e.target.value)} required/>
                     </div>
-                    <div className='Column'>
+                    <div className='Column size40 noMarginRight'>
                         <label>Prénom</label>
                         <input className='InputContact' type="text" name="name" placeholder="Prénom"  value={Prenom} onChange={e => setPrenom(e.target.value)} required/>
                     </div>
@@ -53,26 +53,41 @@ return <>
             </section>
             <section>
                 <h2>Vos coordonées</h2>
-                
+                <div className='Column'>
                     <label>Adresse</label>
                     <input className='InputContact' type="text" name="name" placeholder="adresse"  value={AdressePostale} onChange={e => setAdressePostale(e.target.value)} required/>
-                <label>Code Postal</label>
-                <input className='InputContact' type="number" name="name" placeholder="Code postal"  value={CodePostal} onChange={e => setCodePostal(e.target.value)} required/>
-                <label>Téléphone</label>
-                <input className='InputContact' type="number" name="name" placeholder="Téléphone"  value={Telephone} onChange={e => setTelephone(e.target.value)} required/>
-                <label>Ville</label>
-                <input className='InputContact' type="text" name="name" placeholder="Ville"  value={Ville} onChange={e => setVille(e.target.value)} required/>
-                <label>Courriel</label>
-                <input className='InputContact' type="email" name="name" placeholder="Courriel"  value={Email} onChange={e => setEmail(e.target.value)} required/>
+                </div>
+                <div className='Row1'>
+                    <div className='Column size40 noMarginLeft'>
+                        <label>Code Postal</label>
+                        <input className='InputContact' type="number" name="name" placeholder="Code postal"  value={CodePostal} onChange={e => setCodePostal(e.target.value)} required/>
+                    </div>
+                    <div className='Column size40 noMarginRight'>
+                        <label>Téléphone</label>
+                        <input className='InputContact' type="number" name="name" placeholder="Téléphone"  value={Telephone} onChange={e => setTelephone(e.target.value)} required/>
+                    </div>
+                </div>
+                <div className='Row1'>
+                    <div className='Column size40 noMarginLeft'>
+                        <label>Ville</label>
+                        <input className='InputContact' type="text" name="name" placeholder="Ville"  value={Ville} onChange={e => setVille(e.target.value)} required/>
+                    </div>
+                    <div className='Column size40 noMarginRight'>
+                        <label>Courriel</label>
+                        <input className='InputContact' type="email" name="name" placeholder="Courriel"  value={Email} onChange={e => setEmail(e.target.value)} required/>
+                    </div>
+                </div>
             </section>
             <section>
                 <h2>Votre Message</h2>
                 <label>Objet du message</label>
                 <input className='InputContact' type="text" name="name" placeholder="Objet du message"  value={ObjetMessage} onChange={e => setObjetMessage(e.target.value)} required/>
                 <label>Message</label>
-                <textarea name="name" placeholder="Message" value={Message} onChange={e => setMessage(e.target.value)} required/>
-                <input className='InputContact' type="checkbox" name="name" required/>
-                <label>En envoyant ce formulaire, j’accepte que mes données personnelles soient utilisées pour mener à bien ma demande. En savoir plus *</label>
+                <textarea className='areaMessage' name="name" placeholder="Message" value={Message} onChange={e => setMessage(e.target.value)} required/>
+                <div className='Row'>
+                    <input id='checkContact' type="checkbox" name="name" required/>
+                    <label>En envoyant ce formulaire, j’accepte que mes données personnelles soient utilisées pour mener à bien ma demande. <a href='/404'>En savoir plus</a> *</label>
+                </div>
             </section>
             <button id='buttonContact' type="submit" onClick={sendDataContact}>Envoyer</button>
         </form>
